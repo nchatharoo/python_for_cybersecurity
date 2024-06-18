b2e8e814b16ff7af78e93b876afb3002b2f86239
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-06-18 14:46:35 +0200

    Create a new class to decode IMCP packets

--- sniffer.py
+++ sniffer.py
@@ -32,6 +32,15 @@ class IP:
             print('%s No protocol for %s' % (e, self.protocol_num))
             self.protocol = str(self.protocol_num)
 
+class ICMP:
+    def __init__(self, buff):
+        header = struct.unpack('<BBHHH', buff)
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
+
 def sniff(host):
         # should look familiar from previous example
         if os.name == 'nt':
