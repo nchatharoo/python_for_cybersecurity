bb06845033341eb7ee994599ce5fba31b8bc2f0c
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-06-24 15:36:57 +0200

    change sniff count parameter to 100

--- arper.py
+++ arper.py
@@ -73,7 +73,7 @@ class Arper:
             else:
                 time.sleep(2)
 
-    def sniff(self, count=200):
+    def sniff(self, count=100):
         time.sleep(5)
         print(f'Sniffing {count} packets')
         bpf_filter = "ip host %s" % victim
