e1b0e328aaca5598c775b47f0c1c77a3343a863a
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-06-25 10:41:34 +0200

    Create a method to get the packet header and packet payload

--- recapper.py
+++ recapper.py
@@ -10,3 +10,15 @@ PCAPS = '/root/Downloads'
 
 Response = collections.namedtuple('Response', ['header', 'payload'])
                                   
+def get_header(payload):
+    try:
+       header_raw = payload[:payload.index(b'\r\n\r\n')+2] 
+    except ValueError:
+        sys.stdout.write('-')
+        sys.stdout.flush()
+        return None
+    
+    header = dict(re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', header_raw.decode()))
+    if 'Content-Type' not in header:
+        return None
+    return header
